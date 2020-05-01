9bc08333e51d6e89fb9cb03575ea3cce85e1f343
kfinley <kyle@kylefinley.net>
2020-04-30 20:54:23 -0700

    eslint cleanup wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,26 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    // 'plugin:vue/vue3-recommended',
+    //'plugin:vue/base',
+    "plugin:@typescript-eslint/recommended" // Uses the recommended rules from the @typescript-eslint/eslint-plugin
   ],
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    sourceType: "module", // Allows for the use of imports
+    //parser: "babel-eslint",
+    // ecmaFeatures: {
+    //   jsx: true // Allows for the parsing of JSX
+    // }
   },
-  rules: {    
+  rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',    
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
   overrides: [
     {
