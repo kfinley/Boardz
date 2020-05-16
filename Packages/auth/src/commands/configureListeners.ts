2320a98c7355ad688ec3eb09dc726741a1715f4e
kfinley <kyle@kylefinley.net>
2020-05-15 18:27:21 -0700

    Added: Site Pages md and html wip

--- Packages/auth/src/commands/configureListeners.ts
+++ Packages/auth/src/commands/configureListeners.ts
@@ -3,9 +3,7 @@ import { AuthResponse } from "@/types";
 import { Socket } from "api";
 
 export function configureListeners(commit: Commit) {
-  
   Socket.on("Auth/success", (data: AuthResponse) => {
-    
     commit("Auth/success", data);
   });
 
